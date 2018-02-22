d72584eb25a130f21bb71d04423c5b7619b2ac5c
antreun <antreun@users.noreply.github.com>
2018-02-22 22:17:16 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,25 +1,38 @@
 import socket
 from flask import Flask
 from datetime import datetime
+import os
 
 application = Flask(__name__)
+logfile = "/mnt/storage/log.txt"
+
 
 @application.route("/")
 def hello():
     
-    with open("/mnt/storage/log.txt", "a") as myfile:
+    with open(logfile, "a") as myfile:
         myfile.write(str(datetime.now())+" : "+socket.gethostname()+"\n")
         
         
-    with open("/mnt/storage/log.txt", "r") as myfile:
+    with open(logfile, "r") as myfile:
         data=myfile.read()
         
         
     output = "<html><body><h1>Hello World! Greetings from "+socket.gethostname()+"</h1>\n"
     output += "<h2>Logfile:</h2>\n"
-    output += "<pre>"+data+"</pre></body></html>"
+    output += "<pre>"+data+"</pre>\n"
+    output += "<a href=\"/clearlog\">Click here to clear the log!</a>\n"
     
     return output   
+
+
+@application.route("/clearlog")
+def clearlog():
+
+if os.path.isfile(logfile):
+    os.remove(logfile)
+    
+    return hello()   
 
 
 if __name__ == "__main__":
