18dd5927c8d7157f8a3b43fbafa3039130bdf568
antreun <antreun@users.noreply.github.com>
2018-02-22 22:05:58 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,14 +8,14 @@ application = Flask(__name__)
 def hello():
     
     with open("/mnt/storage/log.txt", "a") as myfile:
-        myfile.write(str(datetime.now())+" : Pod: "+socket.gethostname())
+        myfile.write(str(datetime.now())+" : "+socket.gethostname()+"\n")
         
         
     with open("/mnt/storage/log.txt", "r") as myfile:
         data=myfile.read()
         
         
-    return "Hello World! Greetings from "+socket.gethostname()+"\n\n"+data
+    return "Hello World! Greetings from "+socket.gethostname()+"\n\nLogfile:\n"+data
     
 
 
