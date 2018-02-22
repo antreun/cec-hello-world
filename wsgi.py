0b87d1f6c53a4c083c39b7d4f1c4813f31c719a3
antreun <antreun@users.noreply.github.com>
2018-02-22 22:02:25 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -10,7 +10,13 @@ def hello():
     with open("/mnt/storage/log.txt", "a") as myfile:
         myfile.write(str(datetime.now())+" : Pod: "+socket.gethostname())
         
-    return "Hello World! Greetings from "+socket.gethostname()+"\n"
+        
+    with open("/mnt/storage/log.txt", "r") as myfile:
+        data=myfile.read()
+        
+        
+    return "Hello World! Greetings from "+socket.gethostname()+"\n\n"+data
+    
 
 
 if __name__ == "__main__":
