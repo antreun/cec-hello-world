017cdf1ebb148936bd30261543c243e70c3991cb
antreun <antreun@users.noreply.github.com>
2018-02-22 22:00:27 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,16 @@
 import socket
 from flask import Flask
+from datetime import datetime
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     
-    return "Hello World!!1 Greetings from "+socket.gethostname()+"\n"
+    with open("/mnt/storage/log.txt", "a") as myfile:
+        myfile.write(str(datetime.now())+" : Pod: "+socket.gethostname())
+        
+    return "Hello World! Greetings from "+socket.gethostname()+"\n"
 
 
 if __name__ == "__main__":
